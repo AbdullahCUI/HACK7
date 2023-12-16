3e482e41cd85b4931a6d2c94d88b344c42c028fb
Abdullah-0110 <abdullahdev88@gmail.com>
2023-12-16 22:23:16 +0500

    Function to check if array contains an element within a given range

--- hack7.c
+++ hack7.c
@@ -10,3 +10,18 @@ int contains(const int *arr, int size, int x) {
     }
     return 0; // false
 }
+
+// Function to check if array contains an element within a given range
+int containsWithin(const int *arr, int size, int x, int i, int j) {
+    if (i < 0 || i >= size || j < 0 || j >= size || i > j) {
+        return 0; // false, invalid indices
+    }
+
+    for (int k = i; k <= j; ++k) {
+        if (arr[k] == x) {
+            return 1; // true
+        }
+    }
+    return 0; // false
+}
+
