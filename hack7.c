6696c79f178d0028d52862b407d37ea89d61f9cb
Abdullah-0110 <abdullahdev88@gmail.com>
2023-12-16 22:25:55 +0500

    Function to reverse the elements in the array

--- hack7.c
+++ hack7.c
@@ -43,4 +43,15 @@ int *paddedCopy(const int *arr, int oldSize, int newSize) {
     return newArr;
 }
 
+// Function to reverse the elements in the array
+void reverse(int *arr, int size) {
+    for (int i = 0, j = size - 1; i < j; ++i, --j) {
+        // Swap elements at positions i and j
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+
 
